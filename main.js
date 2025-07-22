775440aade391446ec5585c43a416f303183c6a4
“ElisaGalasso“ <galassoelisa72@gmail.com>
2025-07-22 16:53:04 +0200

    change img on click buttom

---
+++ main.js
@@ -0,0 +1,6 @@
+const firstImgEl = document.querySelector ('img')
+
+const buttonEl = document.querySelector ('button')
+buttonEl.addEventListener('click', function(){
+    firstImgEl.src = './img/yellow_lamp.png'
+})

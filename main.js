f1e4d2baedc5ba29e55e65a43d0d077f6c5478da
“ElisaGalasso“ <galassoelisa72@gmail.com>
2025-07-22 17:17:10 +0200

    add switch on and off

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const firstImgEl = document.querySelector ('img')
 
 const buttonEl = document.querySelector ('button')
-buttonEl.addEventListener('click', function(){
+/*buttonEl.addEventListener('click', function(){
     firstImgEl.src = './img/yellow_lamp.png'
+})*/
+
+buttonEl.addEventListener ('click', function(){
+    if (firstImgEl.src.includes('white_lamp')) {
+        firstImgEl.src =('./img/yellow_lamp.png')
+        buttonEl.textContent = ('Spegni')
+    } else {
+        firstImgEl.src = ('./img/white_lamp.png')
+        buttonEl.textContent = ('Accendi')
+    }
 })
